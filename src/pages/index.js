6bd04cbba3cebb8d0fb49009542117d0d61672c8
Saikat Das <saikatdas0790@gmail.com>
2019-01-16 00:14:20 +0530

    Add HTML, styling and an image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default () => (
-  <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+  <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
 );
